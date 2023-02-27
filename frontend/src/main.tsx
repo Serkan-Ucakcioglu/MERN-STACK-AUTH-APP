9c741886d4ee529e85b496418c1c6bafd69f9845
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-27 15:06:08 +0300

    clear

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import App from "./App";
 import "./index.css";
 import router from "./router/router";
 import { store } from "./store/index";
