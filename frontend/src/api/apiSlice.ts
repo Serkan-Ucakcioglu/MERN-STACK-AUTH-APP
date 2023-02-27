4e6ab81d82df4c16bf1fab9e57a8fe142ed2b074
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-27 15:02:42 +0300

    apiSlice converted typescript

--- frontend/src/api/apiSlice.ts
+++ frontend/src/api/apiSlice.ts
@@ -1,12 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query";
 import { addToken, removeToken } from "../features/authSlice";
+import { RootState } from "../store";
 
-const baseQueryWithReauth = async (args, api, extraOptions) => {
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
   if (result.error && result.error.status === 401) {
     // try to get a new token
     const refreshResult = await baseQuery("/refresh", api, extraOptions);
-    if (refreshResult) {
+    if (refreshResult.data) {
       // store the new token
       api.dispatch(addToken(refreshResult.data));
       // retry the initial query
@@ -17,12 +27,11 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
   }
   return result;
 };
-
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:3001/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
-    const token = getState().authSlice.token;
+    const token = (getState() as RootState).authSlice.token;
 
     if (token) {
       headers.set("authorization", `Bearer ${token}`);
--- frontend/src/components/RequireAuth.tsx
+++ frontend/src/components/RequireAuth.tsx
@@ -1,9 +1,9 @@
-import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 import { selectedToken } from "../features/authSlice";
+import { useAppSelector } from "../hooks/hooks";
 
 function RequireAuth() {
-  const token = useSelector(selectedToken);
+  const token = useAppSelector(selectedToken);
   if (token) {
     return <Outlet />;
   } else {
