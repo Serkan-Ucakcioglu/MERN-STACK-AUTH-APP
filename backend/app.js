59bac4b3aba57eadd4d3187e37e8b2a264a97ecd
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-03-16 13:41:46 +0300

    head title and logo updated

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-
 const cors = require("cors");
 const loginRoute = require("./router/loginRoute");
 const connectDb = require("./config/connect");
